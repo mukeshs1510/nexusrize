cc81921e1654cf27e12adbfcd534106917d344ed
Mukesh Suthar <49620706+mukeshs1510@users.noreply.github.com>
2024-06-25 15:48:09 +0530

    Added live backend base url

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,8 @@ import "./App.css";
 import axios from "axios";
 import MarkdownEditor from "./components/MarkdownEditor";
 
-axios.defaults.baseURL = "http://localhost:8080/";
+// axios.defaults.baseURL = "http://localhost:8080/"; //local server
+axios.defaults.baseURL = "https://nexusrize.onrender.com"; //live server
 
 function App() {
   return (
